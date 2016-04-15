94403883d52d008d40b1290b2699e0d8b580d699
Hu jun <algorist@bjut.edu.cn>
2016-04-15 00:21:05 -0700

    add extern in struct_attr.h for kernel compatiable

--- struct_deal/struct_attr.h
+++ struct_deal/struct_attr.h
@@ -10,16 +10,16 @@
 #define  _CUBE_STRUCT_ATTR_H
 #include "../include/struct_deal.h"
 
-ELEM_OPS string_convert_ops;
-ELEM_OPS estring_convert_ops;
-ELEM_OPS bindata_convert_ops;
-ELEM_OPS define_convert_ops;
-ELEM_OPS int_convert_ops;
-ELEM_OPS uuid_convert_ops;
-ELEM_OPS uuidarray_convert_ops;
-ELEM_OPS defuuidarray_convert_ops;
-ELEM_OPS defnamelist_convert_ops;
-ELEM_OPS enum_convert_ops;
-ELEM_OPS flag_convert_ops;
+extern ELEM_OPS string_convert_ops;
+extern ELEM_OPS estring_convert_ops;
+extern ELEM_OPS bindata_convert_ops;
+extern ELEM_OPS define_convert_ops;
+extern ELEM_OPS int_convert_ops;
+extern ELEM_OPS uuid_convert_ops;
+extern ELEM_OPS uuidarray_convert_ops;
+extern ELEM_OPS defuuidarray_convert_ops;
+extern ELEM_OPS defnamelist_convert_ops;
+extern ELEM_OPS enum_convert_ops;
+extern ELEM_OPS flag_convert_ops;
 
 #endif
